31fadb779aa74b6dc0c7e3c6072dd9f49cade657
Anissa <anissa.elbouazzaoui@gmail.com>
2026-05-22 00:27:11 +0200

    correction

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideRouter, withComponentInputBinding, withHashLocation } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
 
@@ -8,7 +8,7 @@ export const appConfig: ApplicationConfig = {
     // 1. Remove provideZoneChangeDetection
     // 2. Add the zoneless provider:
     provideZonelessChangeDetection(),
-    provideRouter(routes, withComponentInputBinding()),
+    provideRouter(routes, withComponentInputBinding(), withHashLocation()),
     provideHttpClient(withFetch())
   ]
 };
